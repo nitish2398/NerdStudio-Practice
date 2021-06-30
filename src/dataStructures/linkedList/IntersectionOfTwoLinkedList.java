dcc6d7d7a74ddc41c2cbfb4248934ccc6affd758
Nitish Jindal <nijindal@amazon.com>
2021-06-30 17:53:14 +0530

    adding pending questions

--- src/dataStructures/linkedList/IntersectionOfTwoLinkedList.java
+++ src/dataStructures/linkedList/IntersectionOfTwoLinkedList.java
@@ -9,31 +9,63 @@ If the two linked lists have no intersection at all, return null.
 
 public class IntersectionOfTwoLinkedList {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        /*
+        If any one the heads is null
+        then no intersection point exist.
+
+        Return null
+         */
         if (headA == null || headB == null) {
             return null;
         }
 
+        /*
+        Calculate length of both linked lists.
+         */
         int lengthA = lengthOfLinkedList(headA);
         int lengthB = lengthOfLinkedList(headB);
 
+        /*
+        Calculate absolute difference in length of both the lists
+         */
+        int lengthDiff = Math.abs(lengthA - lengthB);
+
+        /*
+        If the length of List-A is greater than List-B
+        Then move head of List-A to make length equal.
+         */
         if (lengthA > lengthB) {
-            for (int i = 0; i < lengthA - lengthB; i++) {
+            for (int i = 0; i < lengthDiff; i++) {
                 headA = headA.next;
             }
         } else {
-            for (int i = 0; i < lengthB - lengthA; i++) {
+            /*
+            Move Head of List-B otherwise.
+             */
+            for (int i = 0; i < lengthDiff; i++) {
                 headB = headB.next;
             }
         }
 
         while (headA != null && headB != null) {
+            /*
+            If both of the heads are pointing to same node,
+            that means this is the intersection point.
+             */
             if (headA.equals(headB)) {
                 return headA;
             }
+
+            /*
+            Move both pointers by one step
+             */
             headA = headA.next;
             headB = headB.next;
         }
 
+        /*
+        Intersection point does not exist, return null
+         */
         return null;
     }
 
